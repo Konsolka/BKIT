9db1e4dad961b2a3ff94c13c80ca6541709c07d8
konsolka <barettianoutov@gmail.com>
2022-10-25 03:52:28 +0300

    created functionality for testing

--- lab_3/lab_python_fp/gen_random.py
+++ lab_3/lab_python_fp/gen_random.py
@@ -1,5 +1,7 @@
 from ctypes import BigEndianStructure
+from distutils.log import error
 import random
+from shutil import ExecError
 
 
 def gen_random(count, begin_num, end_num):
@@ -13,6 +15,8 @@ def gen_random(count, begin_num, end_num):
     Yields:
         int: random number
     """
+    if begin_num > end_num:
+        raise Exception("Error begin number more then end number")
     for i in range(0, count):
         # return random.randint(begin_num, end_num)
         yield random.randint(begin_num, end_num)
@@ -20,4 +24,7 @@ def gen_random(count, begin_num, end_num):
 if __name__ == '__main__':
     gen_f = gen_random(5, 1, 3)
     for i in gen_f:
+        print(i)
+    g = gen_random(5, 5, 1)
+    for i in g:
         print(i)
